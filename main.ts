504d19a34afa53771c5b7515e20b6fc48f325e23
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2020-09-10 00:46:12 +0300

    Send a welcome message when user joins to the server

--- main.ts
+++ main.ts
@@ -7,4 +7,12 @@ bot.on("ready", () => {
 	console.log(`${bot.user?.username} is now online on ${bot.guilds.cache.size} servers!`);
 });
 
+bot.on("message", (message) => {
+	if (message.content.startsWith("++eval")) eval(message.content.replace(/\+\+eval /g, "")); // Added for testing purposes only right now
+});
+
+bot.on("guildMemberAdd", (member) => {
+	member.user?.send(`Merhaba, Bilkent Discord sunucusuna hoş geldin **${member.user?.username}**!`);
+});
+
 bot.login(CONFIG.loginToken);
